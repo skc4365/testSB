cd41f8415eb13e2af10f14dcba2e54c9d2865e38
skc4365 <50658047+skc4365@users.noreply.github.com>
2023-03-06 23:43:35 +0900

    init

---
+++ src/main/java/com/skc/ex4_MVCThymeleaf/Ex4MvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.skc.ex4_MVCThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4MvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4MvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skc/ex4_MVCThymeleaf/Ex4MvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skc.ex4_MVCThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4MvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
