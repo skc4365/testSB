80dddb2ca86f3d717af5d13b14f31ac7a6aae7ad
skc4365 <50658047+skc4365@users.noreply.github.com>
2023-03-07 01:15:03 +0900

    layout 구성

---
+++ src/main/java/com/skc/ex4_MVCThymeleaf/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.skc.ex4_MVCThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
